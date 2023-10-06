cd7314dd70c9ab435b7fca3b339566a18803dc45
ToastBubbles <jeffneal11@gmail.com>
2023-10-06 12:09:04 -0500

    removed redundant lateral connections

--- Scripts/line.cs
+++ Scripts/line.cs
@@ -2,6 +2,7 @@ using DelaunatorSharp;
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 
 public partial class line : Line2D
@@ -15,12 +16,14 @@ public partial class line : Line2D
 	List<IPoint> route = new();
 	Color color = new Color(0, 1, 0, 1);
 	main mainboi;
+	Random rand = new();
 
 	int maxLength = 22;
 	private List<IEdge> goodEdges = new();
 
 	private List<IEdge> badEdges = new();
 	private List<IEdge> longEdges = new();
+	private List<IEdge> optionalEdges = new();
 	public override void _Ready()
 	{
 		mainboi = GetParent().GetParent<main>();
@@ -56,9 +59,11 @@ public partial class line : Line2D
 			Vector2[] badEdgeArr = IEdgeListToV2Array(badEdges);
 			Vector2[] goodEdgeArr = IEdgeListToV2Array(goodEdges);
 			Vector2[] longEdgeArr = IEdgeListToV2Array(longEdges);
+			Vector2[] optEdgeArr = IEdgeListToV2Array(optionalEdges);
 
-			if (badEdgeArr.Length > 0) DrawMultiline(badEdgeArr, new Color(1, 0, 0, 1), 10);
-			if (longEdgeArr.Length > 0) DrawMultiline(longEdgeArr, new Color(0, 0, 1, 1), 10);
+			if (badEdgeArr.Length > 0) DrawMultiline(badEdgeArr, new Color(1, 0, 0, 0.1f), 10);
+			if (longEdgeArr.Length > 0) DrawMultiline(longEdgeArr, new Color(0, 0, 1, 0.1f), 10);
+			if (optEdgeArr.Length > 0) DrawMultiline(optEdgeArr, new Color(1, 1, 0, 0.1f), 10);
 			if (goodEdgeArr.Length > 0) DrawMultiline(goodEdgeArr, color, 10);
 
 			// DrawMultiline(pointsArr, color, 10);
@@ -70,6 +75,7 @@ public partial class line : Line2D
 		}
 
 	}
+
 	private Vector2[] IEdgeListToV2Array(List<IEdge> edgeList)
 	{
 		Vector2[] output = new Vector2[edgeList.Count * 2];
@@ -137,6 +143,7 @@ public partial class line : Line2D
 
 	private void ValidateEdges()
 	{
+		Dictionary<int, int> roomConnections = new Dictionary<int, int>();
 		foreach (IEdge edge in edges)
 		{
 			List<Vector2I> tilesToCheck = new();
@@ -146,8 +153,14 @@ public partial class line : Line2D
 			{
 				isTooLong = true;
 			}
+
+			Room qRoom = null;
+
+			Room pRoom = null;
+
 			foreach (Room room in rooms)
 			{
+				// roomConnections.TryAdd(room.Id, 1);
 				float cx = room.Center.X;
 				float cy = room.Center.Y;
 				bool isQAtCenter = cx == (float)edge.Q.X && cy == (float)edge.Q.Y;
@@ -163,7 +176,16 @@ public partial class line : Line2D
 				}
 				else
 				{
-					GD.Print($"Edge {edge.Q}, {edge.P} touches this room ({room.Center})");
+					if (isPAtCenter)
+					{
+						pRoom = room;
+					}
+					else
+					{
+						qRoom = room;
+					}
+
+					// GD.Print($"Edge {edge.Q}, {edge.P} touches this room ({room.Center})");
 				}
 			}
 
@@ -183,14 +205,290 @@ public partial class line : Line2D
 				else
 				{
 					goodEdges.Add(edge);
+					if (qRoom != null)
+					{
+						AddOrUpdateCount(roomConnections, qRoom.Id);
+					}
+					if (pRoom != null)
+					{
+						AddOrUpdateCount(roomConnections, pRoom.Id);
+					}
 				}
 			}
 
 		}
+		foreach (KeyValuePair<int, int> item in roomConnections)
+		{
+			if (item.Value > 3)
+			{
+				GD.Print($"Room {item.Key} has {item.Value} connections. Too many!");
+				CheckForRedundantEdges(item.Key);
+			}
+			else
+			{
+				GD.Print($"Room {item.Key} has {item.Value} connections");
+			}
+
+
+		}
+
+	}
+	// Dictionary<IPoint, List<IEdge>> edgeGroups = new Dictionary<IPoint, List<IEdge>>();
+
+	// 		foreach (IEdge edge in goodEdges)
+	// 		{
+	// 			// Group by P
+	// 			IPoint keyP = new Point(edge.P.X, edge.P.Y);
+	// 			if (!edgeGroups.ContainsKey(keyP))
+	// 				edgeGroups[keyP] = new List<IEdge>();
+	// 			edgeGroups[keyP].Add(edge);
+
+	// 			// Group by Q
+	// 			IPoint keyQ = new Point(edge.Q.X, edge.Q.Y);
+	// 			if (!edgeGroups.ContainsKey(keyQ))
+	// 				edgeGroups[keyQ] = new List<IEdge>();
+	// 			edgeGroups[keyQ].Add(edge);
+	// 		}
+
+	// 		// Now, you can iterate through the groups to find edges connecting neighboring points
+	// 		foreach (var edgeGroup in edgeGroups.Values)
+	// 		{
+	// 			if (edgeGroup.Count > 1)
+	// 			{
+	// 				// This group contains edges that connect neighboring points
+	// 				// You can add them to your myLatEdges list or perform other actions
+	// 				foreach (var edge in edgeGroup)
+	// 				{
+	// 					// Add to myLatEdges or process as needed
+	// 				}
+	// 			}
+	// 		}
+	private void CheckForRedundantEdges(int roomId)
+	{
+		// Find the room with the specified ID
+		Room room = rooms.FirstOrDefault(x => x.Id == roomId);
+
+		if (room == null)
+		{
+			// Handle the case where the room with the specified ID is not found
+			return;
+		}
+
+		Vector2I roomCenter = room.Center;
+		float cx = roomCenter.X;
+		float cy = roomCenter.Y;
+
+		// Get the edges that are connected to the room center
+		List<IEdge> myEdges = goodEdges
+			.Where(edge => (edge.P.X == cx && edge.P.Y == cy) || (edge.Q.X == cx && edge.Q.Y == cy))
+			.ToList();
+
+		HashSet<IEdge> myLatEdges = new HashSet<IEdge>(); // Use HashSet to store unique edges
+
+		foreach (IEdge edge in myEdges)
+		{
+			GD.PrintRich($"[color=#FF00FF]{roomId} has edge: {GetRoomIdFromV2(IPointToV2(edge.Q))}, {GetRoomIdFromV2(IPointToV2(edge.P))}[/color]");
+		}
+
+		foreach (IEdge edge in goodEdges)
+		{
+			// GD.PrintRich($"[color=#6600FF]Checking edge: {GetRoomIdFromV2(IPointToV2(edge.Q))}, {GetRoomIdFromV2(IPointToV2(edge.P))}[/color]");
+			// bool qMatch = false; //= edge.Q.X == myEdge.Q.X && edge.Q.Y == myEdge.Q.Y;
+			// bool pMatch = false;//edge.P.X == myEdge.P.X && edge.P.Y == myEdge.P.Y;
+			int sharedPoints = 0;
+			foreach (IEdge myEdge in myEdges)
+			{
+
+				if (edge == myEdge)
+				{
+					// Skip comparing an edge with itself
+					GD.PrintRich($"[color=#FF0000]Continuing...[/color]");
+					continue;
+				}
+
+				// if (qMatch == false) qMatch = edge.Q.X == myEdge.Q.X && edge.Q.Y == myEdge.Q.Y;
+				if (edge.Q.X == myEdge.Q.X && edge.Q.Y == myEdge.Q.Y)
+				{
+					// qMatch = true;
+					sharedPoints++;
+				}
+				if (edge.P.X == myEdge.P.X && edge.P.Y == myEdge.P.Y)
+				{
+					// pMatch = true;
+					sharedPoints++;
+				}
+				// if (pMatch == false) pMatch = edge.P.X == myEdge.P.X && edge.P.Y == myEdge.P.Y;
+				GD.PrintRich($"[color=#0088FF]Edge: {GetRoomIdFromV2(IPointToV2(edge.Q))}, {GetRoomIdFromV2(IPointToV2(edge.P))} Checked against myEdge: {GetRoomIdFromV2(IPointToV2(myEdge.Q))}, {GetRoomIdFromV2(IPointToV2(myEdge.P))}[/color]");
+
+				if (sharedPoints > 1)
+				{
+					// Add the redundant edge to myLatEdges HashSet
+					if (!myEdges.Contains(edge) && CanRemoveEdge(edge))// && rand.Next(0, 100) < 50)
+					{
+						GD.PrintRich($"[color=#FFFF00]{roomId} is removing edge: {GetRoomIdFromV2(IPointToV2(edge.Q))}, {GetRoomIdFromV2(IPointToV2(edge.P))}[/color]");
+						myLatEdges.Add(edge);
+
+						optionalEdges.Add(edge);
+					}
+				}
+
+
+			}
+		}
+
+		foreach (IEdge edge in optionalEdges)
+		{
+			goodEdges.Remove(edge);
+		}
+
+		GD.Print("myLatEdges ", myLatEdges.Count);
+		// foreach (IEdge edge in myLatEdges)
+		// {
+		// 	GD.Print($"{edge.Q},{edge.P}");
+
+		// }
+		// ValidateRoomConnections();
 	}
 
+	private int GetRoomIdFromV2(Vector2 pt)
+	{
+
+		return rooms.FirstOrDefault(r => r.Center.X == pt.X && r.Center.Y == pt.Y).Id;
+	}
+
+	// private void ValidateRoomConnections()
+	// {
+
+	// 	List<IEdge> remainingEdges = new(goodEdges);
+	// 	List<IEdge> usedEdges = new();
+	// 	IEdge currentEdge = remainingEdges.First();
+	// 	// IEdge nextEdge;
+	// 	List<int> visitedRooms = new();
+	// 	int i = 0;
+	// 	while (i < goodEdges.Count)
+	// 	{
+	// 		Room rmQ = rooms.FirstOrDefault(x => x.Center.X == currentEdge.Q.X && x.Center.Y == currentEdge.Q.Y);
+	// 		Room rmP = rooms.FirstOrDefault(x => x.Center.X == currentEdge.P.X && x.Center.Y == currentEdge.P.Y);
+	// 		visitedRooms.Add(rmQ.Id);
+	// 		visitedRooms.Add(rmP.Id);
+	// 		usedEdges.Add(currentEdge);
+	// 		remainingEdges.Remove(currentEdge);
+	// 		currentEdge = remainingEdges.FirstOrDefault(e => e.Q.X == currentEdge.Q.X && e.Q.Y == currentEdge.Q.Y || e.P.X == currentEdge.P.X && e.P.Y == currentEdge.P.Y);
+
+	// 		i++;
+
+	// 	}
+	// }
+
+	private bool CanRemoveEdge(IEdge edge)
+	{
+		bool canRemove = false;
 
 
+
+		Room rmQ = rooms.FirstOrDefault(x => x.Center.X == edge.Q.X && x.Center.Y == edge.Q.Y);
+
+		Room rmP = rooms.FirstOrDefault(x => x.Center.X == edge.P.X && x.Center.Y == edge.P.Y);
+
+		if (GetEdgeCount(rmP) > 1 && GetEdgeCount(rmQ) > 1) { canRemove = true; }
+
+		if (!canRemove)
+		{
+			GD.Print("Saved this edge: ", edge.Q, edge.P);
+		}
+
+		return canRemove;
+	}
+	private int GetEdgeCount(Room room)
+	{
+		float cx = room.Center.X;
+		float cy = room.Center.Y;
+		int output = 0;
+
+		foreach (IEdge edge in goodEdges)
+		{
+			bool qMatch = edge.Q.X == cx && edge.Q.Y == cy;
+			bool pMatch = edge.P.X == cx && edge.P.Y == cy;
+
+			if (qMatch || pMatch)
+			{
+				output++;
+			}
+
+		}
+
+
+		return output;
+	}
+	// private void CheckForRedundantEdges(int roomId)
+	// {
+
+	// 	Vector2I roomCenter = rooms.FirstOrDefault(x => x.Id == roomId).Center;
+	// 	float cx = roomCenter.X;
+	// 	float cy = roomCenter.Y;
+	// 	List<Room> neighbors = new();
+
+	// 	List<IEdge> myEdges = goodEdges.Where(edge => edge.P.X == cx && edge.P.Y == cy || edge.Q.X == cx && edge.Q.Y == cy).ToList();
+	// 	List<IEdge> myLatEdges = new();
+	// 	GD.Print("myEdges ", myEdges.Count);
+	// 	foreach (IEdge edge in goodEdges)
+	// 	{
+	// 		foreach (IEdge myEdge in myEdges)
+	// 		{
+
+	// 			bool qMatch = edge.Q.X == myEdge.Q.X && edge.Q.Y == myEdge.Q.Y;
+	// 			bool pMatch = edge.P.X == myEdge.P.X && edge.P.Y == myEdge.P.Y;
+	// 			if (qMatch)
+	// 			{
+
+	// 				foreach (IEdge myEdge2 in myEdges)
+	// 				{
+	// 					if (myEdge2 != myEdge)
+	// 					{
+	// 						bool pMatch2 = edge.P.X == myEdge2.P.X && edge.P.Y == myEdge2.P.Y;
+	// 						if (pMatch2)
+	// 						{
+	// 							myLatEdges.Add(edge);
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 			else if (pMatch)
+	// 			{
+	// 				foreach (IEdge myEdge2 in myEdges)
+	// 				{
+	// 					if (myEdge2 != myEdge)
+	// 					{
+	// 						bool qMatch2 = edge.Q.X == myEdge2.Q.X && edge.Q.Y == myEdge2.Q.Y;
+	// 						if (qMatch2)
+	// 						{
+	// 							myLatEdges.Add(edge);
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+
+	// 	}
+	// 	GD.Print("myLatEdges ", myLatEdges.Count);
+
+
+	// }
+
+
+	private void AddOrUpdateCount(Dictionary<int, int> dictionary, int id)
+	{
+		if (dictionary.ContainsKey(id))
+		{
+			// If the id is already in the dictionary, increment the count
+			dictionary[id]++;
+		}
+		else
+		{
+			// If the id is not in the dictionary, add it with a count of 1
+			dictionary[id] = 1;
+		}
+	}
 	public bool DoesLineIntersectRoom(Vector2 start, Vector2 end, List<Vector2I> tilesToCheck)
 	{
 		foreach (Vector2I tilePos in tilesToCheck)
